ca50a3633e15d3744cc4f0e9c3421ca7c9b9d84a
bloodorange0313 <koudai7272@gmail.com>
2020-04-13 15:19:53 +0900

    157cが解けない

--- abc157/c.py
+++ abc157/c.py
@@ -15,24 +15,33 @@ cnt = 0
 viewing = 1
 ans = [0] * n
 answer = 0
+# まずひとけた目から調べる
+# 重なっていたらすべて調べて、不一致があれば答えは-1
+# 桁が一致するのをすべて調べたら桁上りする(viewing++)。#このとき調べているものも1つ上げる()
+# 見ている桁が調べているものより大きかったら調べているものを1つあげる(cnt++)
 while viewing <= n:
+    if cnt > len(list) - 1:
+        break
     if viewing > list[cnt][0]:
-        viewing += 1
-    elif viewing < list[cnt][0]:
-        ans[viewing] = 0
-        viewing += 1
         cnt += 1
+    elif viewing < list[cnt][0]:
+        ans[viewing - 1] = 0
+        viewing += 1
     elif viewing == list[cnt][0]:
-        tmp = list[cnt][1]
-        changed_val = list[cnt][0]
+        ketabango = list[cnt][0]
+        changed_val = list[cnt][1]
         initial = changed_val
-        ans[cnt] = tmp
-        while  list[cnt][0] == initial:
-            changed_val = list[cnt][0]
-            if changed_val != tmp:
+        ans[viewing - 1] = initial
+        while list[cnt][0] == ketabango:
+            changed_val = list[cnt][1]
+            if changed_val != initial:
+                answer = -1
                 break
             else:
-                cnt += 1
+                if cnt + 1 < len(list):
+                    cnt += 1
+                else:
+                    break
         viewing += 1
     
     else:
@@ -44,5 +53,8 @@ if answer != -1:
     for i in range(len(ans)):
         answer[i] = str(ans[i])
     answer = ''.join(answer)
+    if answer[0] == '0':
+        answer = -1
+
 
 print(answer)
